0703f1a52a8fe605ca856a26f68d40b00b054424
Lysander <19286863+Lysander686@users.noreply.github.com>
2020-12-27 23:01:38 +0800

    page integrated

--- djang3ByExampleBook/c1Blog/blog/views.py
+++ djang3ByExampleBook/c1Blog/blog/views.py
@@ -1,20 +1,41 @@
 from django.shortcuts import render, get_object_or_404
-
+from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
 # Create your views here.
+from django.views.generic import ListView
+
 from .models import Post
 
 
 def post_list(request):
-    posts = Post.published.all()
+    object_list = Post.published.all()
+    paginator = Paginator(object_list, 2)  # 3 posts in each page
+    page = request.GET.get('page')
+    try:
+        posts = paginator.page(page)
+    except PageNotAnInteger:
+        # If page is not an integer deliver the first page
+        posts = paginator.page(1)
+    except EmptyPage:
+        # If page is out of range deliver last page of results
+        posts = paginator.page(paginator.num_pages)
     return render(request,
-                  'blog/post/list.html', {'posts': posts})
+                  'blog/post/list.html',
+                  {'page': page,
+                   'posts': posts})
 
 
 def post_detail(request, year, month, day, post):
     post = get_object_or_404(Post, slug=post,
                              status='published',
-                             publis__year=year,
+                             publish__year=year,
                              publish__month=month,
                              publish__day=day)
 
     return render(request, 'blog/post/detail.html', {'post': post})
+
+
+class PostListView(ListView):
+    queryset = Post.published.all()
+    context_object_name = 'posts'
+    paginate_by = 2
+    template_name = 'blog/post/list.html'
