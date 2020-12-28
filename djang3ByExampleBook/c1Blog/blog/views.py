6f6443a7359d9614391e7d8398026ecad5332bfe
Lysander <19286863+Lysander686@users.noreply.github.com>
2020-12-28 20:09:09 +0800

    show comment success

--- djang3ByExampleBook/c1Blog/blog/views.py
+++ djang3ByExampleBook/c1Blog/blog/views.py
@@ -5,7 +5,7 @@ from django.views.generic import ListView
 
 from c1Blog.appSecret import email_host_user
 from .models import Post
-from .forms import EmailPostForm
+from .forms import EmailPostForm, CommentForm
 from django.core.mail import send_mail
 
 
@@ -39,7 +39,39 @@ def post_detail(request, year, month, day, post):
                              publish__month=month,
                              publish__day=day)
 
-    return render(request, 'blog/post/detail.html', {'post': post})
+    # List of active comments for this post
+    comments = post.comments.filter(active=True)
+
+    new_comment = None
+
+    if request.method == 'POST':
+        # A comment was posted
+        comment_form = CommentForm(data=request.POST)
+        if comment_form.is_valid():
+            # Create Comment object but don't save to database yet
+            new_comment = comment_form.save(commit=False)
+            # Assign the current post to the comment
+            new_comment.post = post
+            # Save the comment to the database
+            new_comment.save()
+    else:
+        comment_form = CommentForm()
+
+    # List of similar posts
+    # post_tags_ids = post.tags.values_list('id', flat=True)
+    # similar_posts = Post.published.filter(tags__in=post_tags_ids) \
+    #     .exclude(id=post.id)
+    # similar_posts = similar_posts.annotate(same_tags=Count('tags')) \
+    #                     .order_by('-same_tags', '-publish')[:4]
+
+    return render(request,
+                  'blog/post/detail.html',
+                  {'post': post,
+                   'comments': comments,
+                   'new_comment': new_comment,
+                   'comment_form': comment_form,
+                   # 'similar_posts': similar_posts
+                   })
 
 
 class PostListView(ListView):
