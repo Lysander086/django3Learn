303b9ff4db1317cf66796aaae3a5662b5eee5083
Lysander <19286863+Lysander686@users.noreply.github.com>
2020-12-29 21:44:58 +0800

    similar posts success

--- djang3ByExampleBook/c1Blog/blog/views.py
+++ djang3ByExampleBook/c1Blog/blog/views.py
@@ -1,36 +1,12 @@
+from django.db.models import Count
 from django.shortcuts import render, get_object_or_404
-from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
-# Create your views here.
-from django.views.generic import ListView
-from taggit.models import Tag
-
-from c1Blog.appSecret import email_host_user
-from .models import Post
-from .forms import EmailPostForm, CommentForm
+from django.core.paginator import Paginator, EmptyPage, \
+    PageNotAnInteger
 from django.core.mail import send_mail
-
-
-def post_share(request, post_id):
-    post = get_object_or_404(Post, id=post_id, status='published')
-    sent = False
-    if request.method == 'POST':
-        form = EmailPostForm(request.POST)
-        if form.is_valid():
-            cd = form.cleaned_data
-            post_url = request.build_absolute_uri(post.get_absolute_url())
-            subject = f"{cd['name']} recommends you read" \
-                      f"{post.title}"
-            message = f"Read {post.title} at {post_url}\n\n" \
-                      f"{cd['name']} \'s comments: {cd['comments']}"
-            send_mail(subject, message, email_host_user, [cd['to']])
-            sent = True
-    else:  # get request
-        form = EmailPostForm()
-    return render(request, 'blog/post/share.html', {
-        'post': post,
-        'form': form,
-        'sent': sent
-    })
+from django.views.generic import ListView
+from .models import Post, Comment
+from .forms import EmailPostForm, CommentForm
+from taggit.models import Tag
 
 
 def post_list(request, tag_slug=None):
@@ -42,7 +18,6 @@ def post_list(request, tag_slug=None):
         object_list = object_list.filter(tags__in=[tag])
 
     paginator = Paginator(object_list, 3)  # 3 posts in each page
-    # get page parameter via url
     page = request.GET.get('page')
     try:
         posts = paginator.page(page)
@@ -85,11 +60,11 @@ def post_detail(request, year, month, day, post):
         comment_form = CommentForm()
 
     # List of similar posts
-    # post_tags_ids = post.tags.values_list('id', flat=True)
-    # similar_posts = Post.published.filter(tags__in=post_tags_ids) \
-    #     .exclude(id=post.id)
-    # similar_posts = similar_posts.annotate(same_tags=Count('tags')) \
-    #                     .order_by('-same_tags', '-publish')[:4]
+    post_tags_ids = post.tags.values_list('id', flat=True)
+    similar_posts = Post.published.filter(tags__in=post_tags_ids) \
+        .exclude(id=post.id)
+    similar_posts = similar_posts.annotate(same_tags=Count('tags')) \
+                        .order_by('-same_tags', '-publish')[:4]
 
     return render(request,
                   'blog/post/detail.html',
@@ -97,12 +72,36 @@ def post_detail(request, year, month, day, post):
                    'comments': comments,
                    'new_comment': new_comment,
                    'comment_form': comment_form,
-                   # 'similar_posts': similar_posts
-                   })
+                   'similar_posts': similar_posts})
 
 
 class PostListView(ListView):
     queryset = Post.published.all()
     context_object_name = 'posts'
-    paginate_by = 2
+    paginate_by = 3
     template_name = 'blog/post/list.html'
+
+
+def post_share(request, post_id):
+    # Retrieve post by id
+    post = get_object_or_404(Post, id=post_id, status='published')
+    sent = False
+
+    if request.method == 'POST':
+        # Form was submitted
+        form = EmailPostForm(request.POST)
+        if form.is_valid():
+            # Form fields passed validation
+            cd = form.cleaned_data
+            post_url = request.build_absolute_uri(post.get_absolute_url())
+            subject = f"{cd['name']} recommends you read {post.title}"
+            message = f"Read {post.title} at {post_url}\n\n" \
+                      f"{cd['name']}\'s comments: {cd['comments']}"
+            send_mail(subject, message, 'admin@myblog.com', [cd['to']])
+            sent = True
+
+    else:
+        form = EmailPostForm()
+    return render(request, 'blog/post/share.html', {'post': post,
+                                                    'form': form,
+                                                    'sent': sent})
