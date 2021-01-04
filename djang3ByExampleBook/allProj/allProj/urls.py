3de485cf173a41af1e74cef2b40549f14c3522dc
Lysander <19286863+Lysander686@users.noreply.github.com>
2021-01-04 21:53:08 +0800

    run success

--- djang3ByExampleBook/allProj/allProj/urls.py
+++ djang3ByExampleBook/allProj/allProj/urls.py
@@ -1,4 +1,4 @@
-"""testDj URL Configuration
+"""allProj URL Configuration
 
 The `urlpatterns` list routes URLs to views. For more information please see:
     https://docs.djangoproject.com/en/3.1/topics/http/urls/
@@ -16,9 +16,7 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path, include
 
-
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('account/', include('account.urls')),
+    path('account/', include('account.urls'))
 ]
-
