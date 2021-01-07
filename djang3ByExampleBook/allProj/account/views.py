0a2db3d52e6f88102a6c0d3e7a3a26fbbbfdc5a5
Lysander <19286863+Lysander686@users.noreply.github.com>
2021-01-07 17:40:00 +0800

    django messages integrated

--- djang3ByExampleBook/allProj/account/views.py
+++ djang3ByExampleBook/allProj/account/views.py
@@ -9,7 +9,7 @@ from django.shortcuts import render
 from django.utils.encoding import force_bytes
 from django.utils.http import urlsafe_base64_encode
 
-from .forms import LoginForm, UserRegistrationForm
+from .forms import LoginForm, UserRegistrationForm, UserEditForm, ProfileEditForm
 
 
 def user_login(request):
@@ -97,8 +97,8 @@ def register(request):
             new_user.set_password(
                 user_form.cleaned_data['password'])
             # Save the User object
-            Profile.objects.create(user=new_user)
             new_user.save()
+            Profile.objects.create(user=new_user)
             return render(request,
                           'account/register_done.html',
                           {'new_user': new_user})
@@ -106,3 +106,25 @@ def register(request):
         user_form = UserRegistrationForm()
 
     return render(request, 'account/register.html', {'user_form': user_form})
+
+
+@login_required
+def edit(request):
+    if request.method == 'POST':
+        
+        user_form = UserEditForm(instance=request.user, data=request.POST)
+
+        profile_form = ProfileEditForm(instance=request.user.profile, data=request.POST, files=request.FILES)
+
+        if user_form.is_valid() and profile_form.is_valid():
+            user_form.save()
+            profile_form.save()
+
+    else:
+        user_form = UserEditForm()
+        profile_form = ProfileEditForm()
+
+    return render(request,
+                  'account/edit.html',
+                  {'user_form': user_form,
+                   'profile_form': profile_form})
---
+++ djang3ByExampleBook/allProj/syntax/basics.py
@@ -0,0 +1,8 @@
+apple = None
+banana = {
+    'color': 'yellow'
+}
+# data =  if apple is not None
+# print(data)
+age = 12
+print('kid' if age < 13 else 'teenager' if age < 18 else 'adult')
